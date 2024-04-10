e6b8f2c2c34b2d357cb16d7d4613e094b6cedd52
sleye2 <sleye2@illinois.edu>
2024-04-09 19:43:47 -0500

    ARTD 218 Final

--- js/script.js
+++ js/script.js
@@ -12,8 +12,9 @@
 
 // ABOUT SLIDER MENU BAR -- AWARD STAR ICON
 
-const toggleMenus = document.getElementById("toggleMenu");
-toggleMenus.addEventListener("click", mobileMenu);
+const toggleMenu = document.getElementById("toggleMenu");
+
+toggleMenu.addEventListener("click", mobileMenu);
 
 let menuBar = document.getElementsByClassName('menu');
 
@@ -21,10 +22,15 @@ function mobileMenu() {
   let menu = document.getElementById("menu");
   menu.classList.toggle("visible");
 
-  if (menuBar.style.visibility === 'visible') {
+  if(menu.classList.contains("visible")){
+    toggleMenu.style.color ='white';
+    toggleMenu.style.marginTop ='30px';
+    toggleMenu.style.marginRight ='30px';
   } else {
-    menu.style.visibility = "visible";
-
+    toggleMenu.style.color ='#d83d3d';
+    toggleMenu.style.marginTop ='0px';
+    toggleMenu.style.marginRight ='0px';
+    
   }
 }
 
@@ -61,40 +67,18 @@ function changeMode() {
     nightModeBtn.style.visibility="visible";
     
     document.getElementById("capBox").style.backgroundColor = "white";
+
+    document.getElementById("toggleMenu").style.color = "#d83d3d";
 }else{
     defaultModeBtn.style.visibility="visible";
     nightModeBtn.style.visibility="hidden";
     
     document.getElementById("capBox").style.backgroundColor = "pink";
 
-}
+    document.getElementById("toggleMenu").style.color = "white";
 
 }
 
+}
 
-// // DAY MODE
 
-// let defaultModeBtn2 = document.querySelector('.default-mode2');
-
-// let dayModeBtn = document.querySelector('.day-mode');
-
-// let page2 = document.querySelector('body');
-
-// defaultModeBtn2.addEventListener('click', changeMode2);
-
-// dayModeBtn.addEventListener('click', changeMode2);
-
-// function changeMode2() {
-//   console.log('this is working');
-//   page2.classList.toggle('state-night-mode');
-
-//   if (defaultModeBtn2.style.visibility === 'visible') {
-//     defaultModeBtn2.style.visibility = "hidden";
-//     dayModeBtn.style.visibility = "visible";
-//   } else {
-//     defaultModeBtn2.style.visibility = "visible";
-//     dayModeBtn.style.visibility = "hidden";
-
-//   }
-
-// }
